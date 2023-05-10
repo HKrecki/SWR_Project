b1f517e9e3aa7c597a5f83275b447af60f0548bd
HKrecki <hubertkrecki@gmail.com>
2023-05-10 09:23:22 +0200

    STM32 project deleted, will be devided to STM32F1 and STM32F4

--- PyQt/main.py
+++ PyQt/main.py
@@ -1,10 +1,32 @@
 from PyQt6.QtWidgets import QApplication, QDialog
 import sys
+import serial
+
+''' Read data and display in terminal
+ser = serial.Serial("COM4", 115000)
+while True:
+    cc = str(ser.readline())
+    print(cc)
 
 app = QApplication(sys.argv)
-
 window = QDialog()
-
 window.show()
-
 sys.exit(app.exec())
+'''
+
+command = input("Write start to begin measurement: ")
+if command == "start":
+    print("Measurement starts")
+    ## Send start to STM32
+
+    ## STM32 resending data
+
+    ## waiting for stop command from user
+
+
+
+
+
+
+
+
