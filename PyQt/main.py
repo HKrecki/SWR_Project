09ac1297e4fcdb6d7199247d94df993c0d23ccbe
HKrecki <hubertkrecki@gmail.com>
2023-05-12 19:11:39 +0200

    Display single image from camera

--- PyQt/main.py
+++ PyQt/main.py
@@ -23,7 +23,6 @@ class Frame_data:
             return True
 
 
-
 # User input to start communication
 start_user_input = "default"
 while start_user_input != "":
@@ -38,65 +37,72 @@ STM32F4.write(b'1')
 frame_started_flag = False
 frame_ended_flag = False
 
-while True:
-    frame = Frame_data
+iterator = 0
 
+# MAIN LOOP - read data from STM #
+while True:
+    # Waiting for frame starts signal: "s"
     data = STM32F4.readline()
 
-    # Frame starts
+    #print(data)
+
+    # Get start frame signal from STM
     if data == b"s\r\r\n":
-        frame.start_symbol = "s"
         frame_started_flag = True
-
-        #aux
-        print("Frame started")
-
-        # Read data in loop and save to 24 lines -> it's itereting 24 times
-        for i in frame.frame_data:
-            print(i)
+        frame_ended_flag = False
+        frame = Frame_data
+        iterator = 0
+        print("Frame starts")
 
 
-'''
-    # Line starts
-    if frame_started_flag == True:
+    if data == b"e\r\r\n" and frame_started_flag == True:
+        frame_started_flag = False
+        frame_ended_flag = True
+
+        print("Frame done")
+        iterator = 0
+
+    if frame_started_flag == True and frame_ended_flag == False:
+        #print(data)
 
         data = str(data)
-        data = data.split(" ")
+        print(len(data))
 
-        if data[0] == "b'sl" and data[-1] == "el\\r\\r\\n'" and len(data) == 34:
-            frame.line_data = data[1:33]
-            #print(frame.line_data)
+        if len(data) >= 204:
+            data_array = data.split(" ")
 
-        print("Frame data: ")
-        print(frame.frame_data)
-'''
+            if "sl" in data_array[0] and "el" in data_array[33]:
+                #print("for iterator: ", iterator, "values: ", data_array[1:32])
+
+                # Save only lines starts at sl end ends at el
+                frame.frame_data[iterator] = data_array[1:33]
+
+                if iterator == 23:
+                    #print(frame.frame_data)
+                    print(len(frame.frame_data))
+                    print(len(frame.frame_data[0]))
+                    frame_np = np.array(frame.frame_data)
+                    frame_np_float = frame_np.astype(float)
+                    print(frame_np_float)
+                    print(type(frame_np_float))
+
+                    plt.imshow(frame_np_float, interpolation="nearest")
+                    plt.show()
+
+
+                #print("Line no. ", iterator, ", values: ", frame.frame_data[iterator])
+
+                iterator = iterator + 1
 
 
 
 
 
+    #if frame_started_flag == True
+    #    frame = Frame_data
+    #    print(data)
 
 
-'''
-img = np.ones((24, 32), dtype = np.uint8)
 
-img[5,:] = [29.41, 28.23, 28.23, 28.95, 28.94, 28.00, 28.81, 28.11, 28.32, 28.28, 28.56, 28.11, 28.69, 27.99, 28.63, 27.97, 28.26, 28.23, 28.43, 28.40, 28.80, 28.60, 28.31, 28.31, 28.83, 28.62, 28.94, 28.74, 29.31, 29.38, 29.23, 28.82]
-
-print('Original Dimensions : ', img.shape)
-
-scale_percent = 500  # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-# resize image
-resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-
-print('Resized Dimensions : ', resized.shape)
-
-cv2.imshow("Resized image", resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
 
 
